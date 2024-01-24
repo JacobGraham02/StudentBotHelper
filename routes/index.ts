d2a5ceaad4fff9c1e4d654f0da859abb1a8f5be7
Jacob Graham <jakegraham54@gmail.com>
2024-01-24 11:09:51 -0500

    Added skeleton functions which will be implemented at a later time with routing logic

--- routes/index.ts
+++ routes/index.ts
@@ -3,8 +3,22 @@ import express, { Request, Response, NextFunction, Router } from 'express';
 const router: Router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req: Request, res: Response, next: NextFunction) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(request: Request, response: Response, next: NextFunction) {
+  response.render('index', 
+  { page_title: "Student Bot Helper portal" 
+  });
+});
+
+router.get('/login', function(request: Request, response: Response, next: NextFunction) {
+  response.render('login', {
+    page_title: "Log in to your portal"
+  });
+});
+
+router.get('/information', function(request: Request, response: Response, next: NextFunction) {
+  response.render('information', {
+    page_title: "Student Bot Helper info"
+  });
 });
 
 export default router;
