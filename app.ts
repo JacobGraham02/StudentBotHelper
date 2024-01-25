aada582a4479189e5cee88544e91f6c506c1bfc5
Jacob Graham <jakegraham54@gmail.com>
2024-01-25 13:00:43 -0500

    Converted all file paths to include ../ at the start of the path. The application attempts to start from within the 'dist' directory, but the 'views' and 'public' directory exists in the root of the folder, which is one directory level above the dist folder

--- app.ts
+++ app.ts
@@ -312,13 +312,13 @@ custom_event_emitter.on('createDiscordGuildEvent',
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'jade');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
 app.use('/users', userRouter);
