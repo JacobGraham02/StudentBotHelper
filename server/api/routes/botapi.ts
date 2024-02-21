09007940c45069915aaccd35af6171fa9e2d6da4
Jacob Graham <jakegraham54@gmail.com>
2024-02-21 11:41:35 -0500

    Completed the function that would allow users to register new commands with the Discord bot by using the /api/registerCommands endpoint

--- server/api/routes/botapi.ts
+++ server/api/routes/botapi.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response, NextFunction, Router } from 'express';
 import RegisterBotCommands from '../controllers/BotCommands/RegisterBotCommands';
 const bot_commands_router: Router = express.Router();
+import * as dotenv from 'dotenv';
+import 'dotenv/config';
+dotenv.config({path: '../../../.env'});
 
 bot_commands_router.post('/registerCommands', function(request: Request, response: Response, next: NextFunction) {
-    
+    const registerBotCommands = new RegisterBotCommands(
+        process.env.discord_bot_token,
+        process.env.discord_bot_application_id,
+        process.env.discord_bot_guild_id
+    );
+    registerBotCommands.pushCommandsIntoCommandList();
+    registerBotCommands.registerCommandsWithDiscordBot(request, response);
 });
 
 export default bot_commands_router;
