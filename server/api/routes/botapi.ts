101987b8917caa3cac8fb91ba17db6a43500bffc
Jacob Graham <jakegraham54@gmail.com>
2024-03-05 11:38:32 -0500

    Modified the backend Logger class to have functionality for logging messages to the bot website. Changed the constructor of the Logger class to allow for flexibility in what variables must be passed into it when the class is being initialized. Modified the botapi route class by adding middleware to both attach the Logger class to a request object, and to verify that request object is not undefined. Added try / catch blocks around a lot of the necessary operations such as writing log messages to Discord.

--- server/api/routes/botapi.ts
+++ server/api/routes/botapi.ts
@@ -3,12 +3,14 @@ import DiscordAPIOperations from '../controllers/BotCommands/DiscordAPIOperation
 const bot_commands_router: Router = express.Router();
 import * as dotenv from 'dotenv';
 import 'dotenv/config';
+import Logger from '../../utils/Logger';
 dotenv.config({path: '../../../.env'});
 
 declare global {
     namespace Express {
         interface Request {
             discordApiOperations: DiscordAPIOperations;
+            logger: Logger;
             botname: string;
         }
     }
@@ -20,16 +22,31 @@ function initializeDiscordApiFunctionsMiddleware(request: Request, response: Res
         process.env.discord_bot_application_id,
         process.env.discord_bot_guild_id
     );
+    const websiteLogger = new Logger();
+    request.logger = websiteLogger;
     request.discordApiOperations = registerBotCommands;
     next();
 }
 
 function ensureDiscordApiOperationsClassNotUndefinedMiddleware(request: Request, response: Response, next: NextFunction): void {
     const discordApiOperations = request.discordApiOperations;
-    if (typeof discordApiOperations === 'undefined') {
+    if (!discordApiOperations) {
         console.error(`The DiscordAPIOperations class obtained from the middleware request is undefined`);
         response.status(500).json({
-            message: `The DiscordAPIOperations class obtained from the middleware request is undefined`
+            message: `The DiscordAPIOperations class obtained from the middleware request is undefined. Please contact the server administrator and inform them of this error`
+        });
+        return;
+    }
+    next();
+}
+
+function ensureLoggerClassNotUndefinedMiddleware(request: Request, response: Response, next: NextFunction): void {
+    const loggerOperations = request.logger;
+
+    if (!loggerOperations) {
+        console.error(`The Logger class obtained from the middleware request is undefined`);
+        response.status(500).json({
+            message: `The Logger class obtained from the middleware request is undefined. Please contact the server administrator and inform them of this error`
         });
         return;
     }
@@ -37,30 +54,41 @@ function ensureDiscordApiOperationsClassNotUndefinedMiddleware(request: Request,
 }
 
 bot_commands_router.post('/registerCommands', initializeDiscordApiFunctionsMiddleware, ensureDiscordApiOperationsClassNotUndefinedMiddleware, 
-    async function(request: Request, response: Response, next: NextFunction) {
-
+    ensureLoggerClassNotUndefinedMiddleware, async function(request: Request, response: Response, next: NextFunction) {
     const discordApiOperations = request.discordApiOperations;
-    if (discordApiOperations) {
+    
+    try {
         await discordApiOperations.pushCommandsIntoCommandList();
         await discordApiOperations.registerCommandsWithDiscordBot(request, response);
+    } catch (error) {
+        console.error(`The bot commands have not been registered. Please inform the server administrator of this error`);
+        throw error;
     }
 });
 
 bot_commands_router.post('/changeDiscordBotName', initializeDiscordApiFunctionsMiddleware, ensureDiscordApiOperationsClassNotUndefinedMiddleware, 
-    async function(request: Request, response: Response, next: NextFunction) {
+    ensureLoggerClassNotUndefinedMiddleware, async function(request: Request, response: Response, next: NextFunction) {
 
     const discordApiOperations = request.discordApiOperations;
-    if (discordApiOperations) {
+
+    try {
         await discordApiOperations.changeDiscordBotName(request, response);
+    } catch (error) {
+        console.error(`There was an error when attempting to change the Discord bot username. Please try again or contact the server administrator if you believe this is an error`);
+        throw error;
     }
 });
 
 bot_commands_router.post('/changeDiscordBotAvatar', initializeDiscordApiFunctionsMiddleware, ensureDiscordApiOperationsClassNotUndefinedMiddleware,
-    async function(request: Request, response: Response, next: NextFunction) {
+    ensureLoggerClassNotUndefinedMiddleware, async function(request: Request, response: Response, next: NextFunction) {
 
     const discordApiOperations = request.discordApiOperations;
-    if (discordApiOperations) {
+
+    try {
         await discordApiOperations.changeDiscordBotAvatar(request, response);
+    } catch (error) {
+        console.error(`There was an error when attempting to change the Discord bot avatar. Please try again or contact the server administrator if you believe this is an error`);
+        throw error;
     }
 });
 
